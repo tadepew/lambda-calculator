c82e6457a704d866d55314bb8cc411a84025c92b
Tristan Depew <tristan.depew@gmail.com>
2019-12-10 21:15:15 -0500

    mvp and styling stretch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Numbers from "../src/components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "../src/components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "../src/components/ButtonComponents/SpecialButtons/Specials";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from "./components/DisplayComponents/Display";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -15,9 +19,12 @@ function App() {
 
   return (
     <div className="container">
-      <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Logo />
+        <Display />
+        <Specials />
+        <Numbers />
+        <Operators />
       </div>
     </div>
   );
